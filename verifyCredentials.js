cf90a4e50033981435c0b60b6ad3007048efc186
Ciprian Serbu <ciprian.serbu@endava.com>
2018-10-10 18:55:59 +0300

    code cleanup + comments

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -7,15 +7,10 @@ module.exports = verify;
 /**
  * Executes an unfiltered search
  *
- * @param credentials object
- * @returns Promise
+ * @param {object} credentials
+ * @returns {Promise}
  */
-console.log('verify credentials loaded.');
 function verify(credentials) {
-    console.log('verifying credentials.....');
     const jira = new JiraConnector(credentials);
-    return jira.request('GET', '/search', {maxResults: 0}).then(() => console.log('verify cred success')).catch((err) => {
-        console.log('verify credentials error!');
-        throw new Error(err);
-    });
+    return jira.request('GET', '/search', {maxResults: 0});
 }
