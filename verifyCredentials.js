41950bed79513877a47bb6725a47115212c3c782
Ciprian Serbu <ciprian.serbu@endava.com>
2018-10-11 13:46:10 +0300

    finished debugging verify creds

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -11,9 +11,7 @@ module.exports = verify;
  * @returns {Promise}
  */
 
-console.log('vc loaded');
 function verify(credentials) {
-    console.log('vc verify');
     const jira = new JiraConnector(credentials);
     return jira.request('GET', '/search', {maxResults: 0});
 }
